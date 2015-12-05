1d1b179b3d4d3eb5bf22ad48ab2f7d2b8376389c
chernosort <chernoglazov96@mail.ru>
2015-12-05 20:04:10 +0300

    Update Motorcycle_ext.h

--- Motorcycle_ext.h
+++ Motorcycle_ext.h
@@ -8,8 +8,12 @@ private:
     int NumberOfWheels;
 public:
     extMotorcycle();
-    extMotorcycle(string Type, string Company, string Model,int Cm3, int HP,int ReleaseYear, long Kilometrage, long Price, int Number);
+    extMotorcycle(string Type, string Company, string Model,int ReleaseYear,int Cm3, int HP, long Kilometrage, long Price, int Number);
     int setNumberOfWheels(int a);
+    int getNumberOfWheels(){return NumberOfWheels;}
+    string toStream();
+    string toFileStream();
+    void setData(string Type, string Company, string Model, int ReleaseYear, int Cm3, int HP, long Kilometrage, long Price, int Number);
 
 };
 
